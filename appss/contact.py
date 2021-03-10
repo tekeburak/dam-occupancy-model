619059d5b1f8169f2cc111f3eb002f8df2a7cb97
Merve Din <merve.din16@gmail.com>
2021-03-10 22:36:19 +0300

    edited contact us page

--- appss/contact.py
+++ appss/contact.py
@@ -8,51 +8,61 @@ from PIL import Image
 def app():
 	st.title('Contact Us')
 	st.markdown("""<p style='text-align: justify;'></p>""", unsafe_allow_html=True)
+	newsize = (300, 300) 
 	
-	image = Image.open('BurakTeke.jpg')
-	st.image(image, width=350)
-	st.subheader('Burak Teke')
+	
+	row1 = st.beta_columns(4)
+	
+	
+	image = Image.open('images/burak.jpg').resize(newsize)
+	row1[0].image(image)
+	row1[0].subheader('Burak Teke')
 	link = '[LinkedIn](https://www.linkedin.com/in/burakteke)'
-	st.markdown(link, unsafe_allow_html=True)
+	row1[0].markdown(link, unsafe_allow_html=True)
 
-	image = Image.open('MerveDin.jpg')
-	st.image(image,width=350)
-	st.subheader('Merve Din')
+
+	image = Image.open('images/merve.jpg').resize(newsize)
+	row1[1].image(image)
+	row1[1].subheader('Merve Din')
 	link = '[LinkedIn](https://www.linkedin.com/in/mervedin16/)'
-	st.markdown(link, unsafe_allow_html=True)
+	row1[1].markdown(link, unsafe_allow_html=True)
 
-	image = Image.open('HaticeKoc.jpg')
-	st.image(image,width=350)
-	st.subheader('Hatice Koç')
+	image = Image.open('images/hatice.jpg').resize(newsize)
+	row1[2].image(image)
+	row1[2].subheader('Hatice Koç')
 	link = '[LinkedIn](https://www.linkedin.com/in/haticekoc/)'
-	st.markdown(link, unsafe_allow_html=True)
+	row1[2].markdown(link, unsafe_allow_html=True)
 
-	image = Image.open('BernadettKepenyesGokcay.jpg')
-	st.image(image,width=350)
-	st.subheader('Bernedett Kepenyes Gökçay')
+	image = Image.open('images/bernadett.jpg').resize(newsize)
+	row1[3].image(image)
+	row1[3].subheader('Bernedett Kepenyes Gökçay')
 	link = '[LinkedIn](https://www.linkedin.com/in/bernadett-kepenyes/)'
-	st.markdown(link, unsafe_allow_html=True)
+	row1[3].markdown(link, unsafe_allow_html=True)
 
-	image = Image.open('MertIzcan.jpg')
-	st.image(image,width=350)
-	st.subheader('Mert İzcan')
-	link = '[LinkedIn](https://www.linkedin.com/in/mert-halit-izcan-834180b1/)'
-	st.markdown(link, unsafe_allow_html=True)
 
-	image = Image.open('SametAydin.jpg')
-	st.image(image,width=350)
-	st.subheader('Samet Aydın')
-	link = '[LinkedIn](https://www.linkedin.com/in/samet-aydin/)'
-	st.markdown(link, unsafe_allow_html=True)
+	row2 = st.beta_columns(4)
 	
-	image = Image.open('OsmanKirci.jpg')
-	st.image(image,width=350)
-	st.subheader('Osman Kırcı')
-	link = '[LinkedIn](https://www.linkedin.com/in/osmank%C4%B1rc%C4%B1)'
-	st.markdown(link, unsafe_allow_html=True)
+	
+	image = Image.open('images/mert.jpg').resize(newsize)
+	row2[0].image(image)
+	row2[0].subheader('Mert İzcan')
+	link = '[LinkedIn](https://www.linkedin.com/in/mert-halit-izcan-834180b1/)'
+	row2[0].markdown(link, unsafe_allow_html=True)
 
-	image = Image.open('HandenurGurpinar.jpeg')
-	st.image(image,width=300)
-	st.subheader('Handenur Gürpınar')
+	image = Image.open('images/samet.jpg').resize(newsize)
+	row2[1].image(image)
+	row2[1].subheader('Samet Aydın')
+	link = '[LinkedIn](https://www.linkedin.com/in/samet-aydin/)'
+	row2[1].markdown(link, unsafe_allow_html=True)
+	
+	image = Image.open('images/osman.jpg').resize(newsize)
+	row2[2].image(image)
+	row2[2].subheader('Osman Kırcı')
+	link = '[LinkedIn](https://www.linkedin.com/in/osmank%C4%B1rc%C4%B1)'
+	row2[2].markdown(link, unsafe_allow_html=True)
+
+	image = Image.open('images/handenur.jpg').resize(newsize)
+	row2[3].image(image)
+	row2[3].subheader('Handenur Gürpınar')
 	link = '[LinkedIn](https://www.linkedin.com/in/handenurgurpinar)'
-	st.markdown(link, unsafe_allow_html=True)
+	row2[3].markdown(link, unsafe_allow_html=True)
