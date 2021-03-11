2874356ae7e70bf8b7105688ea22c15414c93d88
Burak Teke <tr.burakteke@gmail.com>
2021-03-11 23:04:40 +0300

    Add inference

---
+++ inference.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from get_owm_data import get_open_weather_map_data
+
+LSTM_model_name = 'LSTM_model.h5'
+CNN_model_name = 'CNN_model.h5'
+
+model_lstm = tf.keras.models.load_model(LSTM_model_name)
+model_cnn = tf.keras.models.load_model(CNN_model_name)
+
+input = get_open_weather_map_data()
+
+prediction_lstm = model_lstm.predict(input) * 100
+prediction_cnn = model_cnn.predict(input) * 100
