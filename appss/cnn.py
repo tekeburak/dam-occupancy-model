cb5d1983951f56ec922b0dcc3d74c4af10de44b0
Merve Din <merve.din16@gmail.com>
2021-03-11 20:22:22 +0300

    cnn page

--- appss/cnn.py
+++ appss/cnn.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import tensorflow as tf
+from get_owm_data import get_open_weather_map_data
 
 def app():
 	st.title("CNN Model")
@@ -8,3 +10,19 @@ def app():
 	
 	st.subheader('Why we chose CNN?')
 	st.markdown("""<p style='text-align: justify;'>BLABLA</p>""", unsafe_allow_html=True)	
+	
+	CONV_WIDTH = 3
+	LABEL_WIDTH = 30
+	INPUT_WIDTH = 7
+
+	st.write("before model reading")
+	model = tf.keras.models.load_model('cnn_model.h5')
+	st.write("after model reading")
+	
+	"""
+	features = get_open_weather_map_data()
+	st.wirte("after gettin features")
+	prediction = model.predict(features)
+	st.wirte("after gettin preds")
+	st.write(prediction)
+	"""
---
+++ get_owm_data.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import numpy as np
+
+API_KEY = "d36220c2741c4f7076ad680282de5d68"
+LAT = "41.0082"
+LON = "28.9784"
+URL = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&exclude=current,minutely,hourly,alerts&appid=%s&units=metric" % (LAT, LON, API_KEY)
+
+data_size = 7
+
+owm_list = []
+
+def get_open_weather_map_data():
+    response = requests.get(URL)
+    data = json.loads(response.text)
+
+    for i in range(data_size):
+        if 'rain' in data['daily'][i]:
+            Rain = data['daily'][i]['rain']
+        else:
+            Rain = 0.0
+        MaxTemp = data['daily'][i]['temp']['max']
+        MinTemp = data['daily'][i]['temp']['min']
+        AvgWind = data['daily'][i]['wind_speed']
+        AvgHumidity = data['daily'][i]['humidity']
+        AvgPressure = data['daily'][i]['pressure']
+        
+        owm_data = [Rain, MaxTemp, MinTemp, AvgWind, AvgHumidity, AvgPressure]
+        owm_list.append(owm_data)
+        out = np.transpose(np.array(owm_list))
+        out = np.expand_dims(out, axis=0)
+
+    return out
