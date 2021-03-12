b9edc8ad15f6e1d153b679fe96d9160d52edf959
Burak Teke <tr.burakteke@gmail.com>
2021-03-12 14:14:11 +0300

    Update CNN and LSTM models

--- cnn_lstm.py
+++ cnn_lstm.py
@@ -18,9 +18,9 @@ df = pd.read_csv("dam_istanbul_weather.csv", usecols=use_col_list)
 column_indices = {name: i for i, name in enumerate(df.columns)}
 
 n = len(df)
-train_df = df[0:int(n*0.7)]
-val_df = df[int(n*0.7):int(n*0.9)]
-test_df = df[int(n*0.9):]
+train_df = df[0:int(n*0.85)]
+val_df = df[int(n*0.85):int(n*0.95)]
+test_df = df[int(n*0.95):]
 
 train_min = train_df.min()
 train_max = train_df.max()
@@ -38,51 +38,59 @@ MAX_EPOCHS = 1000
 PATIENCE = 200
 
 
-def windowed_dataset(dataFrame, window_size, batch_size, shuffle_buffer):
+# def windowed_dataset(dataFrame, window_size, batch_size):
   
-  dataset = tf.data.Dataset.from_tensor_slices(dataFrame.values)
-  dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
-  dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
-  dataset = dataset.shuffle(shuffle_buffer).map(lambda window: (window[1:], window[0]))
-  dataset = dataset.batch(batch_size).prefetch(1)
+#   dataset = tf.data.Dataset.from_tensor_slices(dataFrame.values)
+#   dataset = dataset.window(window_size + 1, shift=window_size, drop_remainder=True)
+#   dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+#   dataset = dataset.map(lambda window: (window[1::], window[0]))
+#   dataset = dataset.batch(batch_size).prefetch(1)
   
-  return dataset
+#   return dataset
 
-train_dataset = windowed_dataset(train_df, 7, 64, 1000)
-val_dataset = windowed_dataset(val_df, 7, 64, 1000)
-test_dataset = windowed_dataset(test_df, 7, 64, 1000)
+# def parse_samples(x):
+#     return tf.data.Dataset.from_tensor_slices(x)\
+#         .window(size=30, shift=1, drop_remainder=True)\
+#         .flat_map(lambda window: window.batch(30))\
+#         .map(lambda window: (window[1::], window[0]))
 
-def plot(self, model=None, plot_col='GENERAL_DAM_OCCUPANCY_RATE', max_subplots=3):
-  inputs, labels = self.example
-  plt.figure(figsize=(12, 8))
-  plot_col_index = self.column_indices[plot_col]
-  max_n = min(max_subplots, len(inputs))
-  for n in range(max_n):
-    plt.subplot(3, 1, n+1)
-    plt.ylabel(f'{plot_col} [normed]')
-    plt.plot(self.input_indices, inputs[n, :, plot_col_index],
-             label='Inputs', marker='.', zorder=-10)
+train_np = train_df.to_numpy()
+val_np = val_df.to_numpy()
+test_np = test_df.to_numpy()
 
-    if self.label_columns:
-      label_col_index = self.label_columns_indices.get(plot_col, None)
-    else:
-      label_col_index = plot_col_index
+train_np = np.concatenate((train_np[:, 1::], train_np[:, 0, None]), axis=1)
+val_np = np.concatenate((val_np[:, 1::], val_np[:, 0, None]), axis=1)
+test_np = np.concatenate((test_np[:, 1::], test_np[:, 0, None]), axis=1)
 
-    if label_col_index is None:
-      continue
+LABEL_WINDOW_SIZE = 7
 
-    plt.scatter(self.label_indices, labels[n, :, label_col_index],
-                edgecolors='k', label='Labels', c='#2ca02c', s=64)
-    if model is not None:
-      predictions = model(inputs)
-      plt.scatter(self.label_indices, predictions[n, :, label_col_index],
-                  marker='X', edgecolors='k', label='Predictions',
-                  c='#ff7f0e', s=64)
+modified_train_np = train_np[0:-LABEL_WINDOW_SIZE]
+modified_val_np = val_np[0:-LABEL_WINDOW_SIZE]
+modified_test_np = test_np[0:-LABEL_WINDOW_SIZE]
 
-    if n == 0:
-      plt.legend()
+for i in range(LABEL_WINDOW_SIZE-1):
 
-  plt.xlabel('Time [h]')
+  columns_to_append_train = train_np[i+1:-LABEL_WINDOW_SIZE+i+1, -1]
+  columns_to_append_val = val_np[i+1:-LABEL_WINDOW_SIZE+i+1, -1]
+  columns_to_append_test = test_np[i+1:-LABEL_WINDOW_SIZE+i+1, -1]
+
+  modified_train_np = np.append(modified_train_np, columns_to_append_train[:, None], 1)
+  modified_val_np = np.append(modified_val_np, columns_to_append_val[:, None], 1)
+  modified_test_np = np.append(modified_test_np, columns_to_append_test[:, None], 1)
+
+
+train_dataset = tf.keras.preprocessing.timeseries_dataset_from_array(
+    modified_train_np[:, :-LABEL_WINDOW_SIZE], modified_train_np[:, -LABEL_WINDOW_SIZE:], sequence_length=7, batch_size=128)
+
+val_dataset = tf.keras.preprocessing.timeseries_dataset_from_array(
+    modified_val_np[:, :-LABEL_WINDOW_SIZE], modified_val_np[:, -LABEL_WINDOW_SIZE:], sequence_length=7, batch_size=128)
+
+test_dataset = tf.keras.preprocessing.timeseries_dataset_from_array(
+    modified_test_np[:, :-LABEL_WINDOW_SIZE], modified_test_np[:, -LABEL_WINDOW_SIZE:], sequence_length=7, batch_size=128)
+
+# for batch in train_dataset:
+#   inputs, targets = batch
+#   print("Input shape:", inputs.shape, "Target shape:", targets.shape)
 
 
 def compile_and_fit(model, train_dataset, val_dataset, patience=PATIENCE):
@@ -116,35 +124,36 @@ multi_performance = {}
 
 if model_name == 'LSTM':
   model = tf.keras.models.Sequential([
-  tf.keras.layers.Conv1D(filters=60, kernel_size=5,
+  tf.keras.layers.Conv1D(filters=256, kernel_size=5,
   strides=1, padding="causal",
   activation="relu",
-  input_shape=[None, 7]),
-  tf.keras.layers.LSTM(60, return_sequences=True),
-  tf.keras.layers.LSTM(60, return_sequences=True),
-  tf.keras.layers.Dense(30, activation="relu"),
-  tf.keras.layers.Dense(10, activation="relu"),
+  input_shape=[7, 6]),
+  tf.keras.layers.LSTM(128, return_sequences=True),
+  tf.keras.layers.LSTM(64, return_sequences=True),
+  tf.keras.layers.LSTM(32, return_sequences=True),
+  tf.keras.layers.Dense(32, activation="relu"),
+  tf.keras.layers.Dense(16, activation="relu"),
   tf.keras.layers.Dense(1),
   ])
 elif model_name == 'CNN':
   model = tf.keras.models.Sequential([
-  tf.keras.layers.Conv1D(filters=60, kernel_size=5,
+  tf.keras.layers.Conv1D(filters=512, kernel_size=5,
   strides=1, padding="causal",
   activation="relu",
-  input_shape=[None, 7]),
+  input_shape=[7, 6]),
   tf.keras.layers.Conv1D(256, kernel_size=5, strides=1, padding="causal",activation="relu",),
   tf.keras.layers.Conv1D(128, kernel_size=5, strides=1, padding="causal",activation="relu",),
   tf.keras.layers.Conv1D(64, kernel_size=5, strides=1, padding="causal",activation="relu",),
-  tf.keras.layers.Dense(30, activation="relu"),
-  tf.keras.layers.Dense(10, activation="relu"),
+  tf.keras.layers.Dense(32, activation="relu"),
+  tf.keras.layers.Dense(16, activation="relu"),
   tf.keras.layers.Dense(1),
   ])
 
 history = compile_and_fit(model, train_dataset, val_dataset)
 
 
-multi_val_performance[model_name] = model.evaluate(val_dataset)
-multi_performance[model_name] = model.evaluate(test_dataset, verbose=0)
+# multi_val_performance[model_name] = model.evaluate(val_dataset)
+# multi_performance[model_name] = model.evaluate(test_dataset, verbose=0)
 
 model.load_weights(model_ckpt_name)
 
