af37d1cb4a7cc3570ccbc0366aa1e295890244cc
Merve Din <merve.din16@gmail.com>
2021-03-12 15:52:18 +0300

    models are updated

--- get_owm_data.py
+++ get_owm_data.py
@@ -9,17 +9,20 @@ URL = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&exclude=cur
 
 data_size = 7
 
+train_min_values = [0.0, -3.0, -4.0, 2.0, 42.0, 992.0]
+train_max_values = [41.45, 37.0, 26.0, 56.0, 97.0, 1038.0]
+
 
 
 def get_open_weather_map_data():
+    owm_list = []
     response = requests.get(URL)
     data = json.loads(response.text)
-    
-    owm_list = []
 
     for i in range(data_size):
         if 'rain' in data['daily'][i]:
             Rain = data['daily'][i]['rain']
+            Rain = (Rain - train_min_values[0]) / (train_max_values[0] - train_min_values[0])
         else:
             Rain = 0.0
         MaxTemp = data['daily'][i]['temp']['max']
@@ -27,11 +30,16 @@ def get_open_weather_map_data():
         AvgWind = data['daily'][i]['wind_speed']
         AvgHumidity = data['daily'][i]['humidity']
         AvgPressure = data['daily'][i]['pressure']
+
+        MaxTemp = (MaxTemp - train_min_values[1]) / (train_max_values[1] - train_min_values[1])
+        MinTemp = (MinTemp - train_min_values[2]) / (train_max_values[2] - train_min_values[2])
+        AvgWind = (AvgWind - train_min_values[3]) / (train_max_values[3] - train_min_values[3])
+        AvgHumidity = (AvgHumidity - train_min_values[4]) / (train_max_values[4] - train_min_values[4])
+        AvgPressure = (AvgPressure - train_min_values[5]) / (train_max_values[5] - train_min_values[5])
         
         owm_data = [Rain, MaxTemp, MinTemp, AvgWind, AvgHumidity, AvgPressure]
         owm_list.append(owm_data)
-        out = np.transpose(np.array(owm_list))
+        out = np.array(owm_list)
         out = np.expand_dims(out, axis=0)
-        
 
     return out
