8dc5e841282b2a41fc0136dba3362a8549787f54
Merve Din <merve.din16@gmail.com>
2021-03-13 17:16:04 +0300

    removed some codes

--- pages/cnn.py
+++ pages/cnn.py
@@ -12,8 +12,8 @@ def app():
 	st.subheader('What does CNN model do?')
 	st.markdown("""<p style='text-align: justify;'>A Convolutional Neural Network (ConvNet/CNN) is a Deep Learning algorithm which can take in an input image, assign importance (learnable weights and biases) to various aspects/objects in the image and be able to differentiate one from the other.</p>""", unsafe_allow_html=True)
 	
-	st.subheader('Why we chose CNN?')
-	st.markdown("""<p style='text-align: justify;'>BLABLA</p>""", unsafe_allow_html=True)
+	#st.subheader('Why we chose CNN?')
+	#st.markdown("""<p style='text-align: justify;'>BLABLA</p>""", unsafe_allow_html=True)
 
 
 	CNN_model_name = 'models/CNN_model.h5'
--- pages/lstm.py
+++ pages/lstm.py
@@ -10,8 +10,8 @@ def app():
 	st.subheader('What does LSTM model do?')
 	st.markdown("""<p style='text-align: justify;'>Long Short-Term Memory (LSTM) networks are a type of recurrent neural network capable of learning order dependence in sequence prediction problems. An LSTM has a similar control flow as a recurrent neural network. It processes data passing on information as it propagates forward. The differences are the operations within the LSTM's cells. These operations are used to allow the LSTM to keep or forget information.</p>""", unsafe_allow_html=True)
 	
-	st.subheader('Why we chose LSTM?')
-	st.markdown("""<p style='text-align: justify;'>BLABLA</p>""", unsafe_allow_html=True)	
+	#st.subheader('Why we chose LSTM?')
+	#st.markdown("""<p style='text-align: justify;'>BLABLA</p>""", unsafe_allow_html=True)	
 	
 	
 	LSTM_model_name = 'models/LSTM_model.h5'
