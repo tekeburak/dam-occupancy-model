a443536b6f3d554f10e58a0febec751ef1b8fa3d
Burak Teke <tr.burakteke@gmail.com>
2021-03-13 18:48:01 +0300

    LSTM plot updated

--- pages/lstm.py
+++ pages/lstm.py
@@ -2,6 +2,12 @@ import streamlit as st
 import tensorflow as tf
 import numpy
 from utils.get_owm_data import get_open_weather_map_data
+from utils.get_date import get_date_list_for_gmt
+
+import plotly.graph_objects as go
+from plotly import tools
+import plotly.offline as py
+import plotly.express as px
 
 
 def app():
@@ -22,4 +28,19 @@ def app():
 
 
 	prediction_lstm = model_lstm.predict(features) * 100
-	st.line_chart(prediction_lstm.reshape(-1))
+	prediction_lstm = prediction_lstm.ravel()
+	date_list = get_date_list_for_gmt()
+
+	data = []
+
+	layout = go.Layout(
+	
+	title= "<b>LSTM Dam Occupancy Forecasting Plot</b>",paper_bgcolor = 'rgb(248, 248, 255)',plot_bgcolor = 'rgb(248, 248, 255)',barmode = "stack",
+	xaxis = dict(title="Time", linecolor="#BCCCDC",showspikes=True,spikethickness=2,spikedash="dot",spikecolor= "#ffffff",spikemode="across",),
+	yaxis= dict(title="Dam Occupancy Rate (%)",linecolor="#021C1E"))
+
+
+	line_chart= go.Scatter(x=date_list, y=prediction_lstm, marker_color='rgb(0, 200, 200)' )
+	data.append(line_chart)
+	fig= go.Figure(data=data, layout=layout)
+	st.plotly_chart(fig)
