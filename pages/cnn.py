bd7bc056973a5ca2d421f10148bbd3ee51b9aaeb
Burak Teke <tr.burakteke@gmail.com>
2021-03-13 17:29:44 +0300

    cnn description update

--- pages/cnn.py
+++ pages/cnn.py
@@ -10,10 +10,10 @@ def app():
 	st.title("CNN Model")
 	
 	st.subheader('What does CNN model do?')
-	st.markdown("""<p style='text-align: justify;'>A Convolutional Neural Network (ConvNet/CNN) is a Deep Learning algorithm which can take in an input image, assign importance (learnable weights and biases) to various aspects/objects in the image and be able to differentiate one from the other.</p>""", unsafe_allow_html=True)
+	st.markdown("""<p style='text-align: justify;'>Convolutional Neural Network, also known as CNN, is a subfield of deep learning which is mostly used for the analysis of visual imagery. CNN is a class of deep feedforward artificial neural networks (ANN). This Neural Network uses the already supplied dataset for training purposes and predicts the possible future labels to be assigned. Any kind of data This Neural Network uses its strengths against the curse of dimensionality. A portion of the territories where CNNs are broadly utilized are image recognition, image classification, image captioning and object detection etc.</p>""", unsafe_allow_html=True)
 	
-	#st.subheader('Why we chose CNN?')
-	#st.markdown("""<p style='text-align: justify;'>BLABLA</p>""", unsafe_allow_html=True)
+	st.subheader('Why we chose CNN?')
+	st.markdown("""<p style='text-align: justify;'>A convolutional model makes predictions based on a fixed-width history, which may lead to better performance than the dense model since it can see how things are changing over time.</p>""", unsafe_allow_html=True)
 
 
 	CNN_model_name = 'models/CNN_model.h5'
